f1b7e77b5b7984321c63c8b2b01944196eaaadcb
Harro <michael.harrison@cru.org>
2016-02-11 19:30:09 -0500

    Added upload_screenshots logic to projects class

--- lib/onesky/resources/project/file_mgt.rb
+++ lib/onesky/resources/project/file_mgt.rb
@@ -1,3 +1,6 @@
+require 'base64'
+require 'time'
+
 module Resources
   module Project
     module FileMgt
@@ -16,8 +19,60 @@ module Resources
         post_multipart("#{project_path}/files", params)
       end
 
+      def upload_screenshots(params)
+        screenshots = screenshots_from_params(params)
+        body = {:screenshots => screenshots}
+
+        post("#{project_path}/screenshots", body)
+      end
+
       def delete_file(params)
         delete("#{project_path}/files", params)
+      end
+
+      private
+
+      def screenshots_from_params(params)
+
+        return params[:screenshots] if params[:screenshots]
+
+        file = params[:file]
+        folder = params[:folder]
+        tags = params[:tags]
+        since = params[:since]
+        screenshots = []
+
+        if file.is_a?(String) && File.exists?(file) && tags.is_a?(Array) && tags.count > 0
+          tags_for_file = tags.select{ |tag| !tag.has_key?(:file) || tag[:file] == file }
+          screenshots = [screenshot_from_file_tags(file, tags_for_file)]
+        end
+
+        if folder.is_a?(String) && File.directory?(folder) && tags.is_a?(Array) && tags.count > 0
+          screenshots = screenshots_from_folder_tags(folder, tags, since)
+        end
+
+        screenshots
+      end
+
+      def screenshots_from_folder_tags(folder, tags, since)
+        screenshots = []
+        Dir.foreach(folder) { |file|
+          if (File.extname(file) == '.png' || File.extname(file) == '.jpg') && ()!since || File.mtime(file).utc > since.utc)
+            tags_for_file = tags.select{ |tag| !tag.has_key?(:file) || tag[:file] == file }
+            screenshots.push(screenshot_from_file_tags(folder + file, tags_for_file))
+          end
+        }
+
+        screenshots
+      end
+
+      def screenshot_from_file_tags(file, tags)
+        screenshot = []
+        screenshot[:name] = File.basename(file)
+        screenshot[:image] = Base64.encode64(File.open(file, 'rb').read)
+        screenshot[:tags] = tags
+
+        screenshot
       end
 
     end
